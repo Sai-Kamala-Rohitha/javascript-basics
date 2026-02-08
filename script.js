8cb51bbbdd5546083b5e70d37e6fa95c97b29a69
turpati sai kamala rohitha <saikamalarohitha@gmail.com>
2026-02-08 11:42:38 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 alert("Hello! JavaScript is connected.");
 console.log("The script is running smoothly!");
+//varibles
+let name="Rohitha";
+let age=22;
+console.log("Name:",name);
+console.log("Age:",age);
+//simple calculation
+let a=10;
+let b=7;
+console.log("sum:", a+b);
