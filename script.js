bb6778c1c7c90988d0054efb96eab6186f227837
turpati sai kamala rohitha <saikamalarohitha@gmail.com>
2026-02-09 11:12:31 +0530

    Updated script.js
    Added basics of data  types to show how each data type works

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ console.log("Age:",age);
 let a=10;
 let b=7;
 console.log("sum:", a+b);
+// DATA TYPES
+
+// String
+let userName = "Rohitha";
+
+// Number
+let marks = 85;
+let price = 99.5;
+
+// Boolean
+let isStudent = true;
+
+// Undefined
+let city;
+
+// Null
+let phoneNumber = null;
+
+// Display data types
+console.log("userName:", userName, typeof userName);
+console.log("marks:", marks, typeof marks);
+console.log("price:", price, typeof price);
+console.log("isStudent:", isStudent, typeof isStudent);
+console.log("city:", city, typeof city);
+console.log("phoneNumber:", phoneNumber, typeof phoneNumber);
